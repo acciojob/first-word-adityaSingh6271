58e3e65e17b6ebc418af275e2ee1f43cc0ee5c4e
Aditya Singh <131813027+adityaSingh6271@users.noreply.github.com>
2024-01-12 12:01:32 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,16 @@
 function firstWord(s) {
   // your code here
+	 if (str === '' || !str.includes(' ')) {
+    return str;
+  }
+
+  // Find the index of the first space
+  const firstSpaceIndex = str.indexOf(' ');
+
+  // Extract and return the first word up to the first space
+  return str.substring(0, firstSpaceIndex);
 }
+
 
 // Do not change the code below
 
